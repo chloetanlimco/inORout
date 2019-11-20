34e9c8566b38475a94f5f21ceb63a559b6451f40
Chloe Tanlimco <tanlimco@usc.edu>
2019-11-19 17:42:21 -0800

    Revert "everything in correct working directory"
    This reverts commit ecfe46dfbf877b506bdb1eeca40936c921c19f80.

---
+++ src/Register.java
@@ -0,0 +1,104 @@
+
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Register
+ */
+@WebServlet("/Register")
+public class Register extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Register() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		// FRONT END: CHANGE THESE BASED ON YOUR FORM NAMES
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		String confirmpass = request.getParameter("confirmpass");
+		
+		String error = "";
+		if (!password.contentEquals(confirmpass)) {
+			error += "The passwords do not match. ";
+		} 
+		// if username already taken
+		Connection conn = null;
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		
+		try {
+			conn = DriverManager.getConnection("jdbc:mysql://google/foodapp?cloudSqlInstance=groupproject-258805:us-central1:project201&socketFactory=com.google.cloud.sql.mysql.SocketFactory&useSSL=false&user=anthonyuser&password=wQHL223i4LJhEuCl1");
+			st = conn.prepareStatement("SELECT * from users WHERE username=?");
+			st.setString(1, username);
+			rs = st.executeQuery();
+			while (rs.next()) {
+				if (username.contentEquals(rs.getString("username"))) {
+					error += "This username is already taken.";
+				}
+			}
+			
+			// no error
+			if (error.contentEquals("")) {
+				st.execute("INSERT INTO users (username, password) VALUES (\"" + username + "\" , \"" + password + "\")" );
+				// log in user
+				request.getSession().setAttribute("Current User", username);
+			}
+		}
+		catch (SQLException sqle) {
+			System.out.println(sqle.getMessage());
+		}
+			
+
+		request.setAttribute("error", error);
+		
+		// send it back - or forward to a page, whichever
+		RequestDispatcher dispatch = getServletContext().getRequestDispatcher("/Register.jsp");
+		try {
+			dispatch.forward(request, response);
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ServletException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
