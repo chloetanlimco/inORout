e8e02343c36a6d010305dfc0e7759fc592bfb43d
Chloe <shigeoka@usc.edu>
2019-12-02 15:22:24 -0800

    added more categories for businesses

--- src/inORout/Business.java
+++ src/inORout/Business.java
@@ -8,20 +8,25 @@ public class Business {
 	
 	public String id;
 	public String name;
-	public String[] categories;
+	public String categories;
 	public String image_url;
 	public String display_address;
 	public String price;
 	public double rating;
-	public double distance;
+	public double distance = -1;
 	public int review_count;
 	public String display_phone;
 	
 	public Business(JsonObject obj) {
 		// parse
-		System.out.println(obj);
 		id = obj.getAsJsonPrimitive("id").getAsString();
 		name = obj.getAsJsonPrimitive("name").getAsString();
+		JsonArray categorylist = obj.getAsJsonArray("categories");
+		categories = "";
+		for (int i = 0; i < categorylist.size(); i++) {
+			JsonObject category = categorylist.get(i).getAsJsonObject();
+			categories += category.getAsJsonPrimitive("title").getAsString() + ((i == categorylist.size()-1) ? "" : ", ");
+		}
 		image_url = obj.getAsJsonPrimitive("image_url").getAsString();
 		display_address = "";
 		JsonArray addresses = obj.getAsJsonObject("location").getAsJsonArray("display_address"); 
@@ -34,7 +39,9 @@ public class Business {
 			price = "Price unavailable.";
 		}
 		rating = obj.getAsJsonPrimitive("rating").getAsDouble();
-//		distance = obj.getAsJsonPrimitive("distance").getAsDouble();
+		if (obj.getAsJsonPrimitive("distance") != null) {
+			distance = obj.getAsJsonPrimitive("distance").getAsDouble();
+		}
 		review_count = obj.getAsJsonPrimitive("review_count").getAsInt();
 		display_phone = obj.getAsJsonPrimitive("phone").getAsString();
 	}
@@ -45,6 +52,10 @@ public class Business {
 	
 	public String getName() {
 		return name;
+	}
+	
+	public String getCategories() {
+		return categories;
 	}
 	
 	public String getImageUrl() {
